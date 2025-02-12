5a6a8a84bb83fdff315124c93eada5d7ddf568e3
cotontigeh <cotontigeh@gmail.com>
2025-02-12 07:50:03 +0100

    tabLink: add pill variant

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { BiHash, BiTv } from 'react-icons/bi'
 import { TabLink } from './Tabs/TabLink'
 import { Tabs } from './Tabs/Tabs'
+import { Badge } from './Badge/Badge'
 
 function App() {
   return (
@@ -10,9 +12,43 @@ function App() {
         <TabLink active>Buttons</TabLink>
         <TabLink>Badge</TabLink>
       </Tabs>
+
+      <Tabs className="mt-9" variant="noBorder">
+        <TabLink active variant="pill">
+          Buttons
+        </TabLink>
+        <TabLink variant="pill">Badge</TabLink>
+      </Tabs>
+
       <Tabs className="mt-9" variant="noBorder">
         <TabLink>Buttons</TabLink>
         <TabLink active>Badge</TabLink>
+      </Tabs>
+
+      <Tabs className="mt-9">
+        <TabLink>
+          <BiHash />
+          Buttons
+        </TabLink>
+        <TabLink active>
+          <BiTv />
+          Badge
+        </TabLink>
+      </Tabs>
+
+      <Tabs className="mt-9">
+        <TabLink>
+          Buttons
+          <Badge variant="rounded" color="blue">
+            3
+          </Badge>
+        </TabLink>
+        <TabLink active>
+          Badge
+          <Badge variant="rounded" color="blue">
+            1
+          </Badge>
+        </TabLink>
       </Tabs>
     </main>
   )
--- src/Tabs/TabLink.tsx
+++ src/Tabs/TabLink.tsx
@@ -10,12 +10,15 @@ export type TabLinkProps = React.HTMLAttributes<HTMLAnchorElement> &
 
 const tabLinkVariants = cva({
   base: [
-    'flex text-slate-500 font-medium cursor-pointer',
+    'flex gap-2 items-center text-slate-500 font-medium cursor-pointer',
     'border-b-2 border-transparent',
     'py-4 px-1.5',
     'hover:text-slate-600 hover:border-slate-300'
   ],
   variants: {
+    variant: {
+      pill: 'py-2 px-3 rounded-md'
+    },
     active: {
       unset: null,
       true: [
@@ -23,16 +26,23 @@ const tabLinkVariants = cva({
         'hover:border-blue-500 hover:text-blue-500'
       ]
     }
-  }
+  },
+  compoundVariants: [
+    {
+      variant: 'pill',
+      active: true,
+      className: 'bg-gray-100 text-slate-500 border-transparent'
+    }
+  ]
 })
 
 export const TabLink = forwardRef<HTMLAnchorElement, TabLinkProps>(
-  ({ asChild, className, active, ...props }, ref) => {
+  ({ asChild, className, active, variant, ...props }, ref) => {
     const Component = asChild ? Slot : 'a'
     return (
       <Component
         ref={ref}
-        className={cn(tabLinkVariants({ active }), className)}
+        className={cn(tabLinkVariants({ active, variant }), className)}
         {...props}
       />
     )
