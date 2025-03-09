83a2b5a48690b91cd0fd2be49f1ec231c3e76618
cotontigeh <cotontigeh@gmail.com>
2025-03-09 22:23:57 +0100

    feat: add Container component

---
+++ src/ui/Container/Container.tsx
@@ -0,0 +1,23 @@
+import { forwardRef } from 'react'
+import { cn } from '@/lib/utils'
+import { Slot } from '@radix-ui/react-slot'
+
+export type ContainerProps = React.HTMLAttributes<HTMLDivElement> & {
+  asChild?: boolean
+}
+
+export const Container = forwardRef<HTMLDivElement, ContainerProps>(
+  ({ className, asChild, ...props }, ref) => {
+    const Component = asChild ? Slot : 'div'
+
+    return (
+      <Component
+        ref={ref}
+        className={cn('container mx-auto px-8 md:px-0', className)}
+        {...props}
+      />
+    )
+  }
+)
+
+Container.displayName = 'Container'
