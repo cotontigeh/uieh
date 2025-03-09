f9b86d2b21c74eb399a09113dc465620c6c62f84
cotontigeh <cotontigeh@gmail.com>
2025-03-09 21:13:51 +0100

    feat: Backdrop component

---
+++ src/ui/Backdrop/Backdrop.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from 'react'
+import { cn } from '@/lib/utils'
+
+export type BackdropProps = React.HTMLAttributes<HTMLDivElement> & {
+  open?: boolean
+}
+
+export const Backdrop = forwardRef<HTMLDivElement, BackdropProps>(
+  ({ className, open = false, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          open ? 'block' : 'hidden',
+          'absolute z-0 top-0 left-0 w-full h-full bg-black/70',
+          className
+        )}
+        {...props}
+      />
+    )
+  }
+)
+
+Backdrop.displayName = 'Backdrop'
