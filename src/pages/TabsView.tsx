b4d8f226e3587d727034f997449db731be301070
cotontigeh <cotontigeh@gmail.com>
2025-03-21 22:36:11 +0100

    refacto(App): Tabs view

--- src/pages/TabsView.tsx
+++ src/pages/TabsView.tsx
@@ -1,92 +1,131 @@
 import { Tabs } from '@/ui/Tabs/Tabs'
 import { TabLink } from '@/ui/TabLink/TabLink'
 import { BiHash, BiTv } from 'react-icons/bi'
+import { Display } from '@/components/Display'
 
 export const TabsView = () => {
   return (
     <>
       <h1>Tabs</h1>
 
-      <section className="display">
+      <Display>
         <h2>Colors</h2>
-        <Tabs color="blue">
-          <TabLink active>Badge</TabLink>
-          <TabLink>Tab</TabLink>
-        </Tabs>
 
-        <Tabs color="green">
-          <TabLink active>Badge</TabLink>
-          <TabLink>Tab</TabLink>
-        </Tabs>
+        <div>
+          <h3>Base</h3>
+          <Tabs>
+            <TabLink active>Badge</TabLink>
+            <TabLink>Tab</TabLink>
+          </Tabs>
+        </div>
 
-        <Tabs color="red">
-          <TabLink active>Badge</TabLink>
-          <TabLink>Tab</TabLink>
-        </Tabs>
+        <div>
+          <h3>Primary</h3>
+          <Tabs color="primary">
+            <TabLink active>Badge</TabLink>
+            <TabLink>Tab</TabLink>
+          </Tabs>
+        </div>
 
-        <Tabs color="slate">
-          <TabLink active>Badge</TabLink>
-          <TabLink>Tab</TabLink>
-        </Tabs>
-      </section>
+        <div>
+          <h3>Secondary</h3>
+          <Tabs color="secondary">
+            <TabLink active>Badge</TabLink>
+            <TabLink>Tab</TabLink>
+          </Tabs>
+        </div>
 
-      <section className="display">
+        <div>
+          <h3>Accent</h3>
+          <Tabs color="accent">
+            <TabLink active>Badge</TabLink>
+            <TabLink>Tab</TabLink>
+          </Tabs>
+        </div>
+      </Display>
+
+      <Display>
         <h2>No border</h2>
-        <Tabs color="blue" variant="noBorder">
-          <TabLink active>Badge</TabLink>
-          <TabLink>Tab</TabLink>
-        </Tabs>
+        <div>
+          <h3>Base</h3>
+          <Tabs variant="noBorder">
+            <TabLink active>Badge</TabLink>
+            <TabLink>Tab</TabLink>
+          </Tabs>
+        </div>
 
-        <Tabs color="green" variant="noBorder">
-          <TabLink active>Badge</TabLink>
-          <TabLink>Tab</TabLink>
-        </Tabs>
+        <div>
+          <h3>Primary</h3>
+          <Tabs variant="noBorder" color="primary">
+            <TabLink active>Badge</TabLink>
+            <TabLink>Tab</TabLink>
+          </Tabs>
+        </div>
 
-        <Tabs color="red" variant="noBorder">
-          <TabLink active>Badge</TabLink>
-          <TabLink>Tab</TabLink>
-        </Tabs>
+        <div>
+          <h3>Secondary</h3>
+          <Tabs variant="noBorder" color="secondary">
+            <TabLink active>Badge</TabLink>
+            <TabLink>Tab</TabLink>
+          </Tabs>
+        </div>
 
-        <Tabs color="slate" variant="noBorder">
-          <TabLink active>Badge</TabLink>
-          <TabLink>Tab</TabLink>
-        </Tabs>
-      </section>
+        <div>
+          <h3>Accent</h3>
+          <Tabs variant="noBorder" color="accent">
+            <TabLink active>Badge</TabLink>
+            <TabLink>Tab</TabLink>
+          </Tabs>
+        </div>
+      </Display>
 
-      <section className="display">
+      <Display>
         <h2>Pill variant</h2>
-        <Tabs color="blue" variant="noBorder">
-          <TabLink variant="pill" active>
-            Buttons
-          </TabLink>
-          <TabLink variant="pill">Badge</TabLink>
-        </Tabs>
 
-        <Tabs color="green" variant="noBorder">
-          <TabLink variant="pill" active>
-            Buttons
-          </TabLink>
-          <TabLink variant="pill">Badge</TabLink>
-        </Tabs>
+        <div>
+          <h3>Base</h3>
+          <Tabs variant="noBorder">
+            <TabLink variant="pill" active>
+              Badge
+            </TabLink>
+            <TabLink variant="pill">Tab</TabLink>
+          </Tabs>
+        </div>
 
-        <Tabs color="red" variant="noBorder">
-          <TabLink variant="pill" active>
-            Buttons
-          </TabLink>
-          <TabLink variant="pill">Badge</TabLink>
-        </Tabs>
+        <div>
+          <h3>Primary</h3>
+          <Tabs color="primary" variant="noBorder">
+            <TabLink variant="pill" active>
+              Buttons
+            </TabLink>
+            <TabLink variant="pill">Badge</TabLink>
+          </Tabs>
+        </div>
 
-        <Tabs color="slate" variant="noBorder">
-          <TabLink variant="pill" active>
-            Buttons
-          </TabLink>
-          <TabLink variant="pill">Badge</TabLink>
-        </Tabs>
-      </section>
+        <div>
+          <h3>Secondary</h3>
+          <Tabs color="secondary" variant="noBorder">
+            <TabLink variant="pill" active>
+              Buttons
+            </TabLink>
+            <TabLink variant="pill">Badge</TabLink>
+          </Tabs>
+        </div>
 
-      <section className="display">
+        <div>
+          <h3>Accent</h3>
+          <Tabs color="accent" variant="noBorder">
+            <TabLink variant="pill" active>
+              Buttons
+            </TabLink>
+            <TabLink variant="pill">Badge</TabLink>
+          </Tabs>
+        </div>
+      </Display>
+
+      <Display>
         <h2>With decorations</h2>
-        <Tabs color="blue">
+        <Tabs color="primary">
           <TabLink>
             <BiHash />
             Buttons
@@ -97,7 +136,7 @@ export const TabsView = () => {
           </TabLink>
         </Tabs>
 
-        <Tabs color="blue" variant="noBorder">
+        <Tabs color="primary" variant="noBorder">
           <TabLink variant="pill" active>
             <BiHash />
             Buttons
@@ -107,7 +146,7 @@ export const TabsView = () => {
             Badge
           </TabLink>
         </Tabs>
-      </section>
+      </Display>
     </>
   )
 }
