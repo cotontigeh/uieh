5f55c2e11f4cd93a3ec2726a2ea5a7344bf362d9
cotontigeh <cotontigeh@gmail.com>
2025-03-23 00:18:25 +0100

    refacto(Title): Change heading sizes

--- src/ui/Title/title.variants.ts
+++ src/ui/Title/title.variants.ts
@@ -3,15 +3,15 @@ import { cva, VariantProps } from 'cva'
 export type TitleVariantsType = VariantProps<typeof titleVariants>
 
 export const titleVariants = cva({
-  base: ['font-black'],
+  base: ['font-semibold'],
   variants: {
     variant: {
-      h1: ['text-3xl', 'md:text-5xl'],
-      h2: ['text-2xl', 'md:text-4xl'],
-      h3: ['text-xl', 'md:text-3xl'],
-      h4: ['text-lg', 'md:text-2xl'],
-      h5: ['text-base', 'md:text-xl'],
-      h6: ['text-sm', 'md:text-lg']
+      h1: ['font-black text-3xl', 'md:text-5xl'],
+      h2: ['font-bold text-2xl', 'md:text-3xl'],
+      h3: ['text-xl', 'md:text-2xl'],
+      h4: ['text-lg', 'md:text-xl'],
+      h5: ['text-base', 'md:text-lg'],
+      h6: ['text-sm', 'md:text-md']
     },
     uppercase: {
       unset: null,
