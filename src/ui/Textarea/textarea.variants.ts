e5b1624709d303486e4739e83a98bc4c7ac3d1b8
cotontigeh <cotontigeh@gmail.com>
2025-03-22 23:42:33 +0100

    fix(Textarea): remove unecessary classname

--- src/ui/Textarea/textarea.variants.ts
+++ src/ui/Textarea/textarea.variants.ts
@@ -14,38 +14,14 @@ export const textareaVariants = cva({
         'ring-base-300-dark/10 focus:ring-base-300-dark/20',
         'dark:ring-base-300/10 dark:focus:ring-base-300/20'
       ],
-      primary: [
-        'ring-primary/70 focus:ring-primary',
-        'dark:ring-primary/70 dark:focus:ring-primary'
-      ],
-      secondary: [
-        'ring-secondary/70 focus:ring-secondary',
-        'dark:ring-secondary/70 dark:focus:ring-secondary'
-      ],
-      accent: [
-        'ring-accent/70 focus:ring-accent',
-        'dark:ring-accent/70 dark:focus:ring-accent'
-      ],
-      neutral: [
-        'ring-neutral/70 focus:ring-neutral',
-        'dark:ring-neutral/70 dark:focus:ring-neutral'
-      ],
-      info: [
-        'ring-info/70 focus:ring-info',
-        'dark:ring-info/70 dark:focus:ring-info'
-      ],
-      success: [
-        'ring-success/70 focus:ring-success',
-        'dark:ring-success/70 dark:focus:ring-success'
-      ],
-      warning: [
-        'ring-warning/70 focus:ring-warning',
-        'dark:ring-warning/70 dark:focus:ring-warning'
-      ],
-      error: [
-        'ring-error/70 focus:ring-error',
-        'dark:ring-error/70 dark:focus:ring-error'
-      ]
+      primary: ['ring-primary/70 focus:ring-primary'],
+      secondary: ['ring-secondary/70 focus:ring-secondary'],
+      accent: ['ring-accent/70 focus:ring-accent'],
+      neutral: ['ring-neutral/70 focus:ring-neutral'],
+      info: ['ring-info/70 focus:ring-info'],
+      success: ['ring-success/70 focus:ring-success'],
+      warning: ['ring-warning/70 focus:ring-warning'],
+      error: ['ring-error/70 focus:ring-error']
     },
     disabled: {
       unset: null,
