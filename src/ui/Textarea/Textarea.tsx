ceb1d23667cfeef52df27fa670c049fbf2e4e138
cotontigeh <cotontigeh@gmail.com>
2025-03-23 13:47:15 +0100

    feat(Textarea): enhance with headlessUI

--- src/ui/Textarea/Textarea.tsx
+++ src/ui/Textarea/Textarea.tsx
@@ -1,27 +1,26 @@
+import { Textarea as HuTextarea } from '@headlessui/react'
 import { forwardRef } from 'react'
 import { cn } from '@/lib/utils'
 import { textareaVariants, TextareaVariantsType } from './textarea.variants'
 
-export type TextareaProps = Omit<
-  React.InputHTMLAttributes<HTMLTextAreaElement>,
-  'size'
-> &
-  TextareaVariantsType & {}
-
-export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ variant, color, className, disabled, ...props }, ref) => {
-    return (
-      <textarea
-        ref={ref}
-        disabled={disabled}
-        className={cn(
-          textareaVariants({ variant, color, disabled }),
-          className
-        )}
-        {...props}
-      />
-    )
+export type TextareaProps = React.ComponentPropsWithoutRef<typeof HuTextarea> &
+  TextareaVariantsType & {
+    placeholder?: string
+    rows?: number
   }
-)
+
+export const Textarea = forwardRef<
+  React.ComponentRef<typeof HuTextarea>,
+  TextareaProps
+>(({ variant, color, className, disabled, ...props }, ref) => {
+  return (
+    <HuTextarea
+      ref={ref}
+      disabled={disabled}
+      className={cn(textareaVariants({ variant, color, disabled }), className)}
+      {...props}
+    />
+  )
+})
 
 Textarea.displayName = 'Textarea'
