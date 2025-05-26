677249b4e52ce78c0486ec06583eb8373a2e8803
cotontigeh <cotontigeh@gmail.com>
2025-05-27 00:43:16 +0200

    build(types-export): export types on build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({
+      tsconfigPath: './tsconfig.lib.json',
+      rollupTypes: true,
+      insertTypesEntry: true
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
@@ -15,9 +24,11 @@ export default defineConfig({
     lib: {
       entry: {
         uieh: path.resolve(__dirname, 'src/ui/index.ts'),
-        utils: path.resolve(__dirname, 'src/lib/utils.js')
+        utils: path.resolve(__dirname, 'src/lib/utils.ts'),
+        hooks: path.resolve(__dirname, 'src/hooks/index.ts')
       },
-      name: 'uieh'
+      name: 'uieh',
+      formats: ['es']
     },
     rollupOptions: {
       external: ['react', '@headlessui/react'],
