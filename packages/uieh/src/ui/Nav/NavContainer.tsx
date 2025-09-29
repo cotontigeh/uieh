d605d229ed8373a64ed644652b983d797261a82d
cotontigeh <cotontigeh@gmail.com>
2025-09-29 19:05:28 +0200

    refacto(packages/uieh)(Nav): remove useless code for NavContainer

--- packages/uieh/src/ui/Nav/NavContainer.tsx
+++ packages/uieh/src/ui/Nav/NavContainer.tsx
@@ -2,15 +2,10 @@ import { forwardRef } from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cn } from '@/lib/utils'
 import { Container } from '../Container'
-import {
-  navContainerVariants,
-  NavContainerVariantsType
-} from './navContainer.variants'
 
-export type NavContainerProps = React.HTMLAttributes<HTMLDivElement> &
-  NavContainerVariantsType & {
-    asChild?: boolean
-  }
+export type NavContainerProps = React.HTMLAttributes<HTMLDivElement> & {
+  asChild?: boolean
+}
 
 export const NavContainer = forwardRef<HTMLDivElement, NavContainerProps>(
   ({ children, className, color, asChild, ...props }, ref) => {
@@ -20,7 +15,7 @@ export const NavContainer = forwardRef<HTMLDivElement, NavContainerProps>(
       <Container asChild>
         <Component
           ref={ref}
-          className={cn(navContainerVariants({ color }), className)}
+          className={cn('flex gap-4 items-center h-full', className)}
           {...props}
         >
           {children}
