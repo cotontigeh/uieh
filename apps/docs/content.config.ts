f86d46f037de21a9a24fe007878f829e8272d204
cotontigeh <cotontigeh@gmail.com>
2025-10-06 23:05:26 +0200

    feat(apps/docs): new mdx content pages

--- apps/docs/content.config.ts
+++ apps/docs/content.config.ts
@@ -1,5 +1,16 @@
 import { defineCollection, z } from "astro:content";
 
+export type DocsCollectionType = {
+  id: string;
+  slug: string;
+  data: {
+    title: string;
+    author: string;
+    pubDate: Date;
+    updatedDate?: Date;
+  };
+};
+
 export type UiCollectionType = {
   id: string;
   slug: string;
@@ -10,6 +21,16 @@ export type UiCollectionType = {
     updatedDate?: Date;
   };
 };
+
+const docs = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
 
 const ui = defineCollection({
   type: "content",
@@ -23,4 +44,5 @@ const ui = defineCollection({
 
 export const collections = {
   ui,
+  docs,
 };
