be657002216e260c27eca54034c510d43d01cad9
cotontigeh <cotontigeh@gmail.com>
2025-10-06 23:42:53 +0200

    feat(apps/docs): code block styles

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -5,9 +5,23 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [
+    react(),
+    expressiveCode({
+      themes: ["nord", "nord"],
+      styleOverrides: {
+        frames: {
+          frameBoxShadowCssValue: "none",
+        },
+      },
+    }),
+    mdx(),
+  ],
+  markdown: {},
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
